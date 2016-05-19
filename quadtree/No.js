02d9df4aa5b65dcefe14ad2235d08dc8fc58220e
Luiz Fernando <Luiz Fernando>
2016-05-18 22:08:11 -0300

    First

---
+++ quadtree/No.js
@@ -0,0 +1,5 @@
+﻿var No = function () {
+
+};
+
+module.exports = No;
---
+++ server.js
@@ -0,0 +1,14 @@
+﻿var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('Digite os pontos no formato: X,Y ou digite END para encerrar ');
+rl.prompt();
+
+rl.on('line', function (line) {
+    if (line === "END") rl.close();
+    //LOGIC HERE
+    rl.prompt();
+});
+
+rl.on('close', function () {
+    process.exit(0);
+});
