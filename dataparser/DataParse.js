4f8feb449b3398ce98a07bb48d67978c2e430bad
Luiz Fernando <lfernando-silva@hotmail.com>
2016-05-21 23:15:40 -0300

    Add files via upload

--- dataparser/DataParse.js
+++ dataparser/DataParse.js
@@ -1,15 +1,18 @@
 ﻿var fs = require('fs');
 var readline = require('readline');
-var datasourceDirectory = './dataparser/datasets/example/';
+var datasourceDirectory = './dataparser/datasets/';
+var QuadTree = require('../quadtree/QuadTree.js');
+
+var raiz = null;
 
 var DataParse = {
     readTTL: function (file){
         var read = readline.createInterface({
             input: fs.createReadStream(datasourceDirectory + file),
         });
-
         read.on('line', function (line) {
-            return parse(line);
+            var a = read;
+            if (line) parse(line);
         });
     }
 };
@@ -20,9 +23,19 @@ function parse(line){
     if (contains(line, 'point')) {
         //Seleciona apenas as linhas no formato 
         //<http://dbpedia.org/resource/Algeria> <http://www.georss.org/georss/point> "36.7 3.216666666666667"
-        split(line);
+        var data = split(line);
+        if (!raiz) {
+            raiz = QuadTree.insere(null, data.url, data.x, data.y);
+            QuadTree.raiz = raiz;
+        } else {
+            setEncontrado();
+            QuadTree.busca(raiz, data.x, data.y);
+            var encontrado = getEncontrado();
+            if (encontrado === null) { //Se ele não encontrou, então insere na QuadTree
+                QuadTree.insere(raiz, data.url, data.x, data.y);
+            } 
+        }
     }
-    
 }
 
 //Verifica se a linha contém a string "point"
@@ -32,27 +45,33 @@ function contains(line,point){
 
 //Quebra uma linha em: URL, x e y
 function split(line){
-    
+    line = line.replace(' <http://www.georss.org/georss/point> ','').replace('"','');
     var length = line.length;
-    var posURL = [1, line.indexOf('> <')];
-    var posPoint = [line.indexOf('point> '), length];
+    var posURL = [1, line.indexOf('>')];
+    var posPoint = [line.indexOf('>')+1, length-2];
    
     var url = line.slice(posURL[0], posURL[1]);
-    var point = (line.slice(posPoint[0], posPoint[1])).split(' ');
+    var point = line.slice(posPoint[0], posPoint[1]);
+    point = point.split(' ');
     
-    var a = point;
+    var x = parseFloat(point[0]);
+    var y = parseFloat(point[1]);
     
+    return {
+        url: url,
+        x: x,
+        y: y
+    };
+}
 
-    //var x = line.slice(posX[0], posX[1]);
-    //var y = line.slice(line.indexOf(" "), length);
+//Retorna o último ponto encontrado
+function getEncontrado() {
+    return QuadTree.encontrado;
+}
 
-    //var point = {
-    //    url: url,
-    //    x: x,
-    //    y: y
-    //}
-
-    console.log(point);
+//Inicializa o último ponto encontrado = null, sendo que null é quando o nó não foi encontrado.
+function setEncontrado() {
+    QuadTree.encontrado = null;
 }
 
 module.exports = DataParse;
